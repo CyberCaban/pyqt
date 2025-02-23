727d63c77129a52406a5c534b17518b93e5e418e
Dmitry <andrewgamer63@gmail.com>
2025-02-23 11:21:53 +0300

    update base image

--- base/src/main.py
+++ base/src/main.py
@@ -1,11 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from App import Window
 
 
 def main():
     app = QApplication([])
-    # win = Window()
-    # win.show()
+    win = Window()
+    win.show()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
