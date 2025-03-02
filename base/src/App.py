8a341b057a44d1af741f95b4732b2b83f2732a36
Dmitry <andrewgamer63@gmail.com>
2025-03-02 12:49:04 +0300

    Base img upddate

--- base/src/App.py
+++ base/src/App.py
@@ -7,34 +7,21 @@ win_titles = ["Main Window"]
 
 
 class Window(QMainWindow):
-    pages_num = len(win_titles)
-
     def _init_content(self):
-        self.content = QStackedWidget()
-        self.content.addWidget(QLabel("there will be content"))
-
-    def _init_btn(self, page: int):
-        btn = QPushButton(str(page))
-        btn.clicked.connect(lambda: self.navigate(page))
-        return btn
-
-    def _init_navbar(self):
-        self.setWindowTitle(win_titles[0])
-        btns = [self._init_btn(num) for num in range(1, self.pages_num + 1)]
-        navbar_layout = RowLayout(*btns)
-        navbar = QWidget()
-        navbar.setLayout(navbar_layout)
-        return navbar
-
-    def navigate(self, page: int):
-        self.content.setCurrentIndex(page - 1)
-        self.setWindowTitle(win_titles[page - 1])
+        tabs = [
+            {"component": QLabel("Here"), "name": "Sample text"},
+        ]
+        self.content = QTabWidget()
+        self.content.setMovable(True)
+        for tab in tabs:
+            comp = tab["component"]
+            name = tab["name"]
+            self.content.addTab(comp, name)
 
     def __init__(self):
         super().__init__()
         self._init_content()
-        navbar = self._init_navbar()
-        layout = ColLayout(navbar, self.content)
+        layout = ColLayout(self.content)
         main_widget = QWidget()
         main_widget.setLayout(layout)
         self.setCentralWidget(main_widget)
